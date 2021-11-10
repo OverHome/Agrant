44e09bf4c1d27fa0fc479d94d049cb1c2e7d2f59
OverHome <None>
2021-11-10 23:02:12 +0300

    add data base and DBManager

---
+++ DBManager.py
@@ -0,0 +1,98 @@
+import sqlite3
+import hashlib
+
+
+class DBManager:
+
+    def __init__(self):
+        self.title = ["id", "login", "password", "first_name", "last_name", "gender"]
+        self.conn = sqlite3.connect("agrant.db")
+        self.cur = self.conn.cursor()
+
+    def add_user(self, login, fname, lname, password, gender):
+        if not self.in_base(login):
+            pas_hex = self.hesh(password)
+            user_data = (login.lower(), pas_hex, fname.capitalize(), lname.capitalize(), gender)
+            sql_req = f"""
+            INSERT INTO users 
+            ('login', 'password', 'first_name', 'last_name', 'gender') 
+            VALUES(?, ?, ?, ?, ? );
+            """
+            self.cur.execute(sql_req, user_data)
+            self.conn.commit()
+            return "Пользователь создан"
+        else:
+            return "Имя пользователя занято"
+
+    def in_base(self, login):
+        sql_req = f"""SELECT * 
+        FROM users 
+        WHERE login = '{login.lower()}'
+        """
+        return bool(len(list(self.cur.execute(sql_req))))
+
+    def sing_in(self, login, password):
+        if self.in_base(login):
+            user_line = self.find_user_line(login)
+            pas_hex = self.hesh(password)
+            if user_line["password"] == pas_hex:
+                return "Вход одобрен"
+            else:
+                return "Неверный пароль"
+        else:
+            return "Пользователя не существует"
+
+    def find_user_line(self, login):
+        user_dict = {}
+        sql_req = f"""
+        SELECT * 
+        FROM users 
+        WHERE login = '{login.lower()}'
+        """
+        user_line = list(self.cur.execute(sql_req))[0]
+        for i in range(len(self.title)):
+            user_dict[self.title[i]] = user_line[i]
+        return user_dict
+
+    def is_user(self, login, fname, lname):
+        if self.in_base(login):
+            user_line = self.find_user_line(login)
+            if user_line["first_name"] == fname.capitalize() and user_line["last_name"] == lname.capitalize():
+                return "Пользователь подтвержден"
+            else:
+                return "Пользователь не подтвержден"
+        else:
+            return "Пользователя не существует"
+
+    def change_password(self, login, password):
+        if self.in_base(login):
+            pas_hex = self.hesh(password)
+            sql_req = f"""
+            UPDATE users 
+            SET password = '{pas_hex}' 
+            WHERE login = '{login.lower()}'
+            """
+            self.cur.execute(sql_req)
+            self.conn.commit()
+            return "Пароль изменен"
+        else:
+            return "Пользователя не существует"
+
+    def change_user_data(self, login, fname, lname, gender):
+        if self.in_base(login):
+            sql_req = f"""
+            UPDATE users 
+            SET first_name = '{fname}',
+            last_name = '{lname}',
+            gender = '{gender}'
+            WHERE login = '{login.lower()}'
+            """
+            self.cur.execute(sql_req)
+            self.conn.commit()
+            return "Данные акаунта изменены"
+        else:
+            return "Пользователя не существует"
+
+    @staticmethod
+    def hesh(password):
+        return hashlib.sha1(bytes(password, encoding="UTF-8")).hexdigest()
